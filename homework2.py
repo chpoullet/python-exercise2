00a9394fad9b75f7e408d67ddc19d9476b5a31fe
Charles Poullet <CPoullet@spartaglobal.com>
2020-02-05 11:02:46 +0000

    Added some methods

--- homework2.py
+++ homework2.py
@@ -7,9 +7,10 @@ from random import randint
 secret_num = randint(1, 10)
 
 print('Hello there, please choose a number between 1-10!')
-user_num = int(input())
+user_num = int(input().strip())
 
 if user_num == secret_num:
     print('Well done, you won!')
 else:
     print('Unlucky, maybe next time!')
+    print(f'The number was actually {secret_num}.')
